1fb2e60c7600e51dbe78d8fbf698822e3e20f032
asad-23 <140097856+asad-23@users.noreply.github.com>
2024-06-25 23:51:45 +0600

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy: {
       // '/api': 'http://localhost:5000'
-      '/api': 'https://mernstackfoodgo.onrender.com',
+      '/api': 'https://foodgo-back.onrender.com',
     }
   },
   plugins: [react()],
